243f2c17d2dfd7c00666bb0abe79985a30fe8309
VitorgsRuffo <vitorruffo19cc@gmail.com>
2019-10-01 18:59:51 -0300

    Implementing menu first option (Need debugging).

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define Q_contatos 50
+#define Q_contatos 5
+
 
 typedef struct{
     char nome[50];
@@ -12,8 +13,88 @@ typedef struct{
     int On;
 }contatos;
 
-void add(){
-    system("cls");
+
+typedef struct{
+    contatos contato[Q_contatos];
+    int contactsalreadyadded;
+}contact_list;
+
+void init_contatos(contact_list *Contact_List);
+void add(contact_list *Contact_List);
+void menu(contact_list *Contact_List);
+
+
+int main (void){
+    FILE *arq;
+
+    contact_list Contact_List;
+
+    init_contatos(&Contact_List);
+
+    menu(&Contact_List);
+
+
+    return 0;
+}
+
+
+void init_contatos(contact_list *Contact_List){
+    for(int i; i<Q_contatos; ++i)
+        (*Contact_List).contato[i].On = 0;
+    
+    (*Contact_List).contactsalreadyadded = 0;
+}
+
+void add(contact_list *Contact_List){
+    int adding;
+
+    for(int i = 0; i>=Q_contatos; ++i){
+
+        for(int k = 0; k<Q_contatos; ++k){
+            (*Contact_List).contactsalreadyadded = 0;
+            
+            if((*Contact_List).contato[k].On == 1)
+                ++(*Contact_List).contactsalreadyadded;
+        }
+
+        if ((*Contact_List).contactsalreadyadded >= Q_contatos){
+            system("cls");
+            printf("-----------------------------------\n");
+            printf("|          NOVO CONTATO:          |\n");
+            printf("|---------------------------------|\n");
+            printf("\n  Limite de contatos atingido!\n");
+            getch();
+            break;
+        }
+
+        if((*Contact_List).contato[i].On == 0){
+            system("cls");
+            printf("-----------------------------------\n");
+            printf("|          NOVO CONTATO:          |\n");
+            printf("|---------------------------------|\n");
+            printf("Nome: \n");
+            scanf(" %[^\n]s", (*Contact_List).contato[i].nome);
+            printf("Numero (1): \n");
+            scanf(" %s", (*Contact_List).contato[i].numero[0]);
+            printf("Numero (2): \n");
+            scanf(" %s", (*Contact_List).contato[i].numero[1]);
+            printf("Endereco: \n");
+            scanf(" %[^\n]s", (*Contact_List).contato[i].endereco);
+            printf("Email: \n");
+            scanf(" %s", (*Contact_List).contato[i].email);
+            (*Contact_List).contato[i].On = 1; 
+                     
+
+            printf("\n\nDeseja adicionar outro contato?\n1 - Sim.\n2 - Nao.\n");
+            scanf("%d", &adding);
+
+            if(adding == 2)
+                break;
+                
+        }
+    
+    }
+    
 }
 
 void editar(){
@@ -35,21 +116,26 @@ void buscar(){
 }
 
 
-void menu(contatos *contato){
-    system("cls");
+void menu(contact_list *Contact_List){
+    
     int option;
 
-        do{
-        printf("1 - Adicionar novo contato\n");
-        printf("2 - Editar\n");
-        printf("3 - Excluir\n");
-        printf("4 - Listar\n");
-        printf("5 - Buscar\n");
-        printf("0 - Sair\n");
+    do{
+        system("cls");
+        printf("-----------------------------------\n");
+        printf("|       LISTA DE CONTATOS:        |\n");
+        printf("|                                 |\n");
+        printf("| 1 - Adicionar novo contato.     |\n");
+        printf("| 2 - Editar.                     |\n");
+        printf("| 3 - Excluir.                    |\n");
+        printf("| 4 - Listar.                     |\n");
+        printf("| 5 - Buscar.                     |\n");
+        printf("| 0 - Sair                        |\n");
+        printf("-----------------------------------\n");
         scanf("%d", &option);
         switch (option){
             case 1:
-                add();
+                add(Contact_List);
                 break;
             case 2:
                 editar();
@@ -66,13 +152,4 @@ void menu(contatos *contato){
         }
     }while(option != 0);
 
-}
-
-
-int main (void){
-    contatos contato[Q_contatos];
-    menu(contato);
-
-
-    return 0;
 }
